a360cf43c223a6a119d7bd22a6c56b1826e0b346
assig <assigdev@gmail.com>
2017-02-14 18:04:57 +0300

    update

---
+++ js/soida.js
@@ -0,0 +1,156 @@
+ var CirclePaint = {
+    // options vars
+     cross:{
+         strokeColor: 'black'
+     },
+     sin_style:{
+         strokeColor: 'blue',
+         strokeWidth: 2
+     },
+    width: 2400,
+    part_width: 1200,
+    height:400,
+    part_height: 200,
+    hatch: {
+        count: 36,
+        length: 3,
+        bigLength: 6,
+        styles: {
+            color: 'black',
+            bigColor: 'red'
+        }
+    },
+    text: {
+        indent: {
+            left: 30,
+            right: 30
+        },
+        styles: {
+            fillColor: 'black',
+            fontSize: '15px'
+
+        },
+        smallStyles: {
+            fillColor: 'green',
+            fontSize: '10px'
+        }
+    },
+
+     _hatch_draw: function (start, end, color) {
+        var path = new Path();
+        path.strokeColor = color;
+        path.add(start);
+        path.add(end);
+    },
+
+    _text_draw: function (endHatch, textContent, styles, angle) {
+
+        text = new PointText(endHatch);
+        text.fontSize = styles.fontSize;
+        text.fillColor = styles.fillColor;
+        text.justification = 'center';
+        text.content = textContent.toString();
+        return text;
+    },
+
+     sin_draw: function () {
+         var i,
+             rad = 0,
+             j=30,
+             y=0,
+            center=new Point(this.part_width, this.part_height),
+            sin = new Path();
+         sin.add(center);
+         sin.strokeColor = this.sin_style.strokeColor;
+         for(i=this.width/2+30; i<=this.width-30; i+=30){
+             rad = j*Math.PI/180;
+             var sin_ugla = Math.sin(rad).toFixed(1);
+             if (sin_ugla<0){
+                 y= sin_ugla * -150 +200;
+             }
+             else {
+                 y = (1-sin_ugla) * 150 + 50;
+             }
+             sin.add(new Point(i,y));
+             sin.smooth();
+
+             console.log(Math.sin(rad).toFixed(1));
+             console.log(j);
+             console.log(y);
+             j+=30;
+         }
+
+     },
+     cos_draw: function () {
+         var i,
+             rad = 0,
+             j=30,
+             y=0,
+            center=new Point(this.part_width, 50),
+            sin = new Path();
+         sin.add(center);
+         sin.strokeColor = this.sin_style.strokeColor;
+         for(i=this.width/2+30; i<=this.width-30; i+=30){
+             rad = j*Math.PI/180;
+             var sin_ugla = Math.cos(rad).toFixed(1);
+             if (sin_ugla<0){
+                 y= sin_ugla * -150 +200;
+             }
+             else {
+                 y = (1-sin_ugla) * 150 + 50;
+             }
+             sin.add(new Point(i,y));
+             sin.smooth();
+
+             console.log(Math.cos(rad).toFixed(1));
+             console.log(j);
+             console.log(y);
+             j+=30;
+         }
+
+     },
+
+     cross_draw: function(){
+      this._pl_draw(new Point(this.part_width, 50), new Point(this.part_width, 350) , 'x');
+      this._pl_draw(new Point(0, this.part_height), new Point(this.width, this.part_height) , 'y');
+    },
+    _pl_draw: function (topPoint, bottomPoint, coord) {
+        var i,j;
+        var cross = new Path();
+        cross.add(topPoint);
+        cross.add(bottomPoint);
+        cross.strokeColor = this.cross.strokeColor;
+        if (coord === 'x') {
+            j = 30;
+            for(i=this.width/2+30; i<=this.width-30; i+=30){
+                this._hatch_draw(new Point(i, this.part_height+4), new Point(i, this.part_height-4), 'black');
+                this._text_draw( new Point(i, this.part_height+15),j,this.text.smallStyles, false);
+                j+=30;
+            }
+            j = -30;
+            for(i=this.width/2-30; i>=30; i-=30){
+                this._hatch_draw(new Point(i, this.part_height+4), new Point(i, this.part_height-4), 'black');
+                this._text_draw( new Point(i, this.part_height-15),j,this.text.smallStyles, false);
+                j-=30;
+            }
+        }
+        else {
+            j =0.9;
+            for(i=65; i<=335; i+=15){
+                this._hatch_draw(new Point(this.part_width+4, i), new Point(this.part_width-4, i), 'black');
+                if(j>0) {
+                    this._text_draw(new Point(this.part_width+15, i + 5), j, this.text.smallStyles, false);
+                }
+                else if(j<0){
+                    this._text_draw(new Point(this.part_width-15, i + 5), j, this.text.smallStyles, false);
+                }
+                j-=0.1;
+                j = j.toFixed(1);
+            }
+        }
+
+    }
+};
+
+CirclePaint.cross_draw();
+CirclePaint.cos_draw();
--- js/transportir.js
+++ js/transportir.js
@@ -1,7 +1,7 @@
 /**
  * Created by assig on 26.01.2017.
  */
-
+var drag_count=0, prev_angle=0, drag_points =0, angles=[];
 var CirclePaint = {
     // options vars
     circle: {
@@ -11,6 +11,14 @@ var CirclePaint = {
             radius: 150,
             strokeColor: 'black',
             fillColor: 'white'
+        }
+    },
+    angle_number: {
+        circle_style:{
+            center: new Point(380, 200),
+            radius: 20,
+            strokeColor: '#3385ff',
+            fillColor: '#3385ff'
         }
     },
     hatch: {
@@ -40,9 +48,21 @@ var CirclePaint = {
     arrow: {
         styles: {
             strokeColor: 'red',
-            strokeWidth: 6
+            strokeWidth: 10
         }
+
     },
+
+    arrow_points: [
+        new Point(320, 200),
+        new Point(80, 200),
+        new Point(290, 200),
+        new Point(110, 200),
+        new Point(260, 200),
+        new Point(140, 200),
+        new Point(230, 200),
+        new Point(170, 200)
+    ],
 
     // init vars
     circleObj: null,
@@ -66,6 +86,7 @@ var CirclePaint = {
         this.endHatchVector.length -= this.hatch.length;
         this.bigStartHatchVector.length += this.hatch.bigLength;
         this.bigEndHatchVector.length -= this.hatch.bigLength;
+
     },
 
     draw : function () {
@@ -79,7 +100,7 @@ var CirclePaint = {
                 startHatch = this.bigStartHatchVector + this.circle.styles.center;
                 endHatch = this.bigEndHatchVector + this.circle.styles.center;
                 this._hatch_draw(startHatch, endHatch, this.hatch.styles.bigColor);
-                this._text_draw(endHatch, angle_number, this.text.styles, true);
+                angles[i]=this._text_draw(endHatch, angle_number, this.text.styles, true);
             }
             else {
                 this.startHatchVector.angle = angle_number;
@@ -87,7 +108,7 @@ var CirclePaint = {
                 startHatch = this.startHatchVector + this.circle.styles.center;
                 endHatch = this.endHatchVector + this.circle.styles.center;
                 this._hatch_draw(startHatch, endHatch, this.hatch.styles.color);
-                this._text_draw(endHatch, angle_number, this.text.smallStyles, true);
+                angles[i]=this._text_draw(endHatch, angle_number, this.text.smallStyles, true);
             }
 
         }
@@ -98,7 +119,6 @@ var CirclePaint = {
         path.strokeColor = color;
         path.add(start);
         path.add(end);
-        path.closed = true;
     },
 
     _text_draw: function (endHatch, textContent, styles, angle) {
@@ -109,15 +129,9 @@ var CirclePaint = {
 
 
             textVector = endHatch - this.circle.styles.center;
-
-            if (endHatch.x < this.circle.styles.center.x) {
-                textVector.length += this.text.indent.left;
-            }
-            else {
-                textVector.length += this.text.indent.right;
-            }
-
+            textVector.length += this.text.indent.right;
             textPoint = textVector + this.circle.styles.center;
+            textContent += '\u00B0';
         }
         else {
             textPoint = endHatch;
@@ -127,46 +141,64 @@ var CirclePaint = {
         text.fillColor = styles.fillColor;
         text.justification = 'center';
         text.content = textContent.toString();
+        return text;
     },
+
+    _angle_number_draw: function () {
+      var number_group = new Group();
+        var numb_circle = new Path.Circle(this.angle_number.circle_style);
+
+        text = new PointText(this.angle_number.circle_style.center);
+            text.fontSize = '18px';
+            text.fillColor = 'white';
+            text.justification = 'center';
+            text.content = '0\u00B0';
+            text.position.y+=7;
+        number_group.addChild(numb_circle);
+        number_group.addChild(text);
+        return number_group;
+        },
+
     arrow_generate: function () {
         var arrow = new Path;
         var center = this.circle.styles.center;
-        arrow.strokeColor =  this.arrow.styles.strokeColor;
+        arrow.strokeColor =  '#F0FFFF';
         arrow.strokeWidth = this.arrow.styles.strokeWidth;
         arrow.add(center);
         arrow.add(this.circle.leftSide);
-        // arrow.closed = true;
+        var red_arrow = arrow.clone();
+            red_arrow.strokeColor = 'red';
+            red_arrow.strokeWidth = 2;
+            arrow.opacity = 0.1;
         var sin = new Path();
-        sin.add(center);
-        sin.add(center);
-        sin.strokeColor = 'blue';
+            sin.strokeWidth = 4;
+            sin.add(center);
+            sin.add(center);
+            sin.strokeColor = 'blue';
         var cos = sin.clone();
-        arrow.onMouseDrag = function (event) {
-            var event_vector = event.point - center;
-            var arrow_vector = arrow.segments[1].point - center;
-            arrow_vector.angle = event_vector.angle;
-            // ugol.innerText = event_vector.angle;
-            var endHatch = arrow_vector + center;
-            arrow.segments[1].point = endHatch;
 
-            sin.segments[0].point.y = endHatch.y;
-            sin.segments[1].point = endHatch;
-            cos.segments[0].point.x = endHatch.x;
-            cos.segments[1].point = endHatch;
-            var y = sin.segments[0].point.y;
-            y = 200 - y;
-            y /=150;
-            sin_text.innerText = y.toFixed(3);
-            var x = cos.segments[0].point.x;
-            x = x-200;
-            x /=150;
-            cos_text.innerText = x.toFixed(3);
 
-            var audio = new Audio('assets/tik4.mp3 ');
-            if(parseInt(arrow_vector.angle.toFixed(2))%10==0){
-                audio.play();
-            }
+
+        var lines = [];
+        for(var i=0; i<8; i++) {
+            var line = new Path();
+                line.strokeColor = 'green';
+                line.strokeWidth = 4;
+                for(var j=0; j<11; j++) {
+                    line.add(this.arrow_points[i]);
+                }
+            lines[i]=line;
         }
+
+        var group_number = this._angle_number_draw();
+        arrow.onMouseDrag = function (event) {
+
+            arrow_move(event, arrow, red_arrow, sin, cos, center, lines, group_number);
+        };
+        red_arrow.onMouseDrag = function (event) {
+            arrow_move(event, arrow, red_arrow, sin, cos, center, lines, group_number);
+        };
+
     },
     cross_draw: function(){
       this._pl_draw(new Point(200, 50), new Point(200, 350) , 'x');
@@ -203,3 +235,127 @@ CirclePaint.cross_draw();
 CirclePaint.draw();
 
 CirclePaint.arrow_generate();
+
+
+
+function arrow_move(event, arrow, red_arrow, sin, cos, center, lines, number_group)  {
+
+    var event_vector = event.point - center;
+    var arrow_vector = arrow.segments[1].point - center;
+    arrow_vector.angle = event_vector.angle;
+    // ugol.innerText = event_vector.angle;
+    var endHatch = arrow_vector + center;
+
+    arrow.segments[1].point = endHatch;
+    red_arrow.segments[1].point = endHatch;
+
+
+    sin.segments[0].point.y = endHatch.y;
+    sin.segments[1].point = center;
+    cos.segments[0].point.x = endHatch.x;
+    cos.segments[1].point = center;
+    var y = sin.segments[0].point.y;
+    y = 200 - y;
+    y /=150;
+    sin_text.innerText = y.toFixed(3);
+    var x = cos.segments[0].point.x;
+    x = x-200;
+    x /=150;
+    cos_text.innerText = x.toFixed(3);
+
+    var audio = new Audio('assets/tik4.mp3 ');
+    if(parseInt(arrow_vector.angle.toFixed(2))%10==0){
+        audio.play();
+    }
+    drag_line(event, arrow_vector.angle);
+    line_paint(lines, arrow_vector, center);
+
+    var number_group_vector = arrow_vector.clone();
+        number_group_vector.length = arrow_vector.length + 30;
+    number_group.position = number_group_vector + center;
+    number_group.children[1].content=parseInt(drag_count)+ '\u00B0';
+}
+
+
+function drag_line(event, angle) {
+    var raz_angle;
+    if(angle<=0) {
+        if(prev_angle>20) {
+                prev_angle = prev_angle - 360;
+        }
+       raz_angle = angle - prev_angle;
+       raz_angle *= -1;
+        drag_count += raz_angle;
+    }
+    else{
+       if(prev_angle<-20) {
+           prev_angle = 360+prev_angle;
+       }
+        raz_angle = prev_angle - angle;
+
+        drag_count += raz_angle;
+    }
+    prev_angle = angle;
+    drag_points = parseInt(drag_count/180);
+
+}
+
+function line_paint(lines, arrow_vector, center) {
+    var razn_arrow = 30;
+    var line = lines[Math.abs(drag_points)];
+    change_angles();
+    switch (Math.abs(drag_points)){
+        case 2:
+        case 3: razn_arrow = 60; break;
+        case 4:
+        case 5: razn_arrow = 90; break;
+        case 6:
+        case 7: razn_arrow = 120;
+    }
+    var line_vector = arrow_vector.clone();
+        line_vector.length = line_vector.length-razn_arrow;
+        line.segments[10].point = line_vector + center;
+    var middle_line_vector = line_vector.clone();
+    var i, angle;
+        if (line_vector.angle<=0 && drag_count>=0 || line_vector.angle>0 && drag_count<0) {
+            for (i = 1; i < 10; i++) {
+                middle_line_vector.angle = line_vector.angle * 0.1 * i;
+                line.segments[i].point = middle_line_vector + center;
+            }
+        }
+        else if(line_vector.angle<=0 && drag_count<0){
+             angle = -180 - line_vector.angle;
+            for (i = 1; i < 10; i++) {
+                middle_line_vector.angle = -180 - angle * 0.1 * i;
+                line.segments[i].point = middle_line_vector + center;
+
+            }
+        }
+        else {
+            angle = 180 - line_vector.angle;
+            for (i = 1; i < 10; i++) {
+                middle_line_vector.angle = 180 - angle * 0.1 * i;
+                line.segments[i].point = middle_line_vector + center;
+
+            }
+        }
+        // line.selected = true;
+
+        line.smooth({ type: 'catmull-rom' });
+
+    var before_line = lines[Math.abs(drag_points-1)]
+
+
+}
+
+
+    function change_angles() {
+        console.log(drag_points+' change_angles');
+        var points = drag_points;
+        if(drag_points<0) points -=1;
+        if (points%2===0)
+        for(var i=1; i<=CirclePaint.hatch.count; i++) {
+            angles[i].content = 10*i+360*points/2;
+            angles[i].content += '\u00B0';
+        }
+    }
